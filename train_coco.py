58dbc01307b7890644fd91f1e659606431f8ac1f
LEE-JAEHA <years6666@gmail.com>
2021-02-13 22:12:54 +0900

    tensorboardx add

--- train_coco.py
+++ train_coco.py
@@ -8,14 +8,28 @@ import numpy as np
 import random
 import torch.backends.cudnn as cudnn
 import argparse
+from pycocotools.cocoeval import COCOeval
 from coco_eval import COCOGenerator
+import coco_eval
 from torchsummary import summary
+
+from tensorboardX import SummaryWriter
+summary = SummaryWriter()
+
+loss_list = ['cls_loss','cnt_loss','reg_loss','total_loss']
+# summary.add_scalar('learning_rate', lr, epoch)
+# summary.add_scalar('loss_cls', loss[0], epoch)
+# summary.add_scalar('loss_centerness', loss[1], epoch)
+# summary.add_scalar('loss_regression', loss[2], epoch)
+# summary.add_scalar('loss_total', loss[3], epoch)
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--epochs", type=int, default=48, help="number of epochs")
-parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
+parser.add_argument("--batch_size", type=int, default=16, help="size of each image batch")
 parser.add_argument("--n_cpu", type=int, default=2, help="number of cpu threads to use during batch generation")
-parser.add_argument("--n_gpu", type=str, default='0', help="number of cpu threads to use during batch generation")
+parser.add_argument("--n_gpu", type=str, default='0,1', help="number of cpu threads to use during batch generation")
 opt = parser.parse_args()
 
 os.environ["CUDA_VISIBLE_DEVICES"]=opt.n_gpu
@@ -30,12 +44,13 @@ random.seed(0)
 transform = Transforms()
 
 resize_ = [600,600]
-train_dataset=COCODataset("/HDD/jaeha/dataset/COCO/2014/train2014",
-                          '/HDD/jaeha/dataset/COCO/2014/annotations/instances_train2014.json',transform=transform,resize_size=resize_)
-
-
-val_dataset=COCOGenerator("/HDD/jaeha/dataset/COCO/2017/train2017",
+# train_dataset=COCODataset("/HDD/jaeha/dataset/COCO/2014/train2014",
+#                           '/HDD/jaeha/dataset/COCO/2014/annotations/instances_train2014.json',transform=transform,resize_size=resize_)
+train_dataset=COCODataset("/HDD/jaeha/dataset/COCO/2017/train2017",
+                          '/HDD/jaeha/dataset/COCO/2017/annotations/changed500_instances_train2017.json',transform=transform,resize_size=resize_)
+val_dataset=COCOGenerator("/HDD/jaeha/dataset/COCO/2017/val2017",
                           '/HDD/jaeha/dataset/COCO/2017/annotations/changed500_instances_val2017.json',resize_size=resize_)
+
 
 # changed_val_dataset=COCOGenerator("/HDD/jaeha/dataset/COCO/2017/val2017",
 #                           '/HDD/jaeha/dataset/COCO/2017/annotations/changed_instances_train2014.json',resize_size=resize_)
@@ -47,6 +62,8 @@ model = FCOSDetector(mode="training").cuda()
 print(model)
 
 model = torch.nn.DataParallel(model)
+# model.load_state_dict(torch.load("./checkpoint/model_{}.pth".format(21), map_location=torch.device('cuda:0')))
+
 BATCH_SIZE = opt.batch_size
 EPOCHS = opt.epochs
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True,
@@ -58,9 +75,16 @@ WARMUP_STEPS = 500
 WARMUP_FACTOR = 1.0 / 3.0
 GLOBAL_STEPS = 0
 # LR_INIT = 0.01 #origin
-LR_INIT = 0.005
+LR_INIT = 0.01
 optimizer = torch.optim.SGD(model.parameters(), lr=LR_INIT, momentum=0.9, weight_decay=0.0001)
 lr_schedule = [120000, 160000]
+eval_list = ["AP5095","AP50","AP75","AP_SMALL","AP_MED","AP_LARGE","AR_5095","AR50","AR75","AR_SMALL","AR_MED","AR_LARGE"]
+data_ = dict()
+for idx,key in enumerate(eval_list):
+    summary.add_scalar(key,1, idx)
+print(data_)
+# summary.add_scalar("Accuracy",data_, 1)
+input("TIME")
 
 
 def lr_func(step):
@@ -101,34 +125,48 @@ for epoch in range(EPOCHS):
 
         optimizer.zero_grad()
         losses = model([batch_imgs, batch_boxes, batch_classes])
+
+
         loss = losses[-1]
         loss.mean().backward()
+        # import pdb
+        # pdb.set_trace()
+
         torch.nn.utils.clip_grad_norm(model.parameters(), 3)
         optimizer.step()
 
         end_time = time.time()
         cost_time = int((end_time - start_time) * 1000)
-        if epoch_step % 100 == 0:
-            print(
-                "global_steps:%d epoch:%d steps:%d/%d cls_loss:%.4f cnt_loss:%.4f reg_loss:%.4f cost_time:%dms lr=%.4e total_loss:%.4f" % \
-                (GLOBAL_STEPS, epoch + 1, epoch_step + 1, steps_per_epoch, losses[0].mean(), losses[1].mean(),
-                 losses[2].mean(), cost_time, lr, loss.mean()))
+        # if epoch_step % 100 == 0:
+        print(
+            "global_steps:%d epoch:%d steps:%d/%d cls_loss:%.4f cnt_loss:%.4f reg_loss:%.4f cost_time:%dms lr=%.4e total_loss:%.4f" % \
+            (GLOBAL_STEPS, epoch + 1, epoch_step + 1, steps_per_epoch, losses[0].mean(), losses[1].mean(),
+             losses[2].mean(), cost_time, lr, loss.mean()))
         GLOBAL_STEPS += 1
-    torch.save(model.state_dict(), "./checkpoint/model_{}.pth".format(epoch + 1))
 
-    if epoch + 1 > 20:
-        model2 = FCOSDetector(mode="inference")
-        model2 = torch.nn.DataParallel(model2)
-        model2 = model2.cuda().eval()
-        model2.load_state_dict(
-            torch.load("./checkpoint/model_{}.pth".format(epoch + 1), map_location=torch.device('cuda:1')),
-            strict=False)
-        tt = coco_eval.evaluate_coco(val_dataset, model2)
-        m_acc = tt[4].astype(float)
-        if m_acc > best_acc:
-            best_acc = m_acc
-            best_ep = epoch + 1
-        print("Best Acc of Medium : {0}, Best Ep of Medium : {1}".format(best_acc, best_ep))
+    for idx in range(4):
+        summary.add_scalar(loss_list[idx], losses[idx][0], epoch)
+    torch.save(model.state_dict(), "./checkpoint2/model_{}.pth".format(epoch + 1))
+
+    # if epoch + 1 > 23:
+    model2 = FCOSDetector(mode="inference")
+    model2 = torch.nn.DataParallel(model2)
+    model2 = model2.cuda().eval()
+    model2.load_state_dict(
+        torch.load("./checkpoint2/model_{}.pth".format(epoch + 1), map_location=torch.device('cuda:1')),
+        strict=False)
+    tt = coco_eval.evaluate_coco(val_dataset, model2)
+    m_acc = tt[4].astype(float)
+    if m_acc > best_acc:
+        best_acc = m_acc
+        best_ep = epoch + 1
+
+    data_ = dict()
+    for idx,key in enumerate(eval_list):
+        summary.add_scalar(key, tt[idx].astype(float), epoch)
+    print("Best Acc of Medium : {0}, Best Ep of Medium : {1}".format(best_acc, best_ep))
+
+summary.close()
 
 
 
